8058dcfc9fdd0dd8b742348a1f9a16d76a569cf9
unknown <danny30lin@gmail.com>
2019-07-17 23:25:07 +0800

    callback

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
+import json as js
+import requests as rq
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+@app.route("/", methods = ["GET"])
 def hello():
     return "Hi web!!"
 
-@app.route("/test", methods=['GET', 'POST'])
-def test():
-    return "Hi test!!"
+@app.route("/callback", methods = ["POST"])
+def callback():
+    line_json = rq.get_json()
+    return "line post!!"
 
 if __name__ == "__main__":
     app.run()
