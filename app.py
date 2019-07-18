28902256f0a7693e8c736cbc7733542e4fd3dce5
unknown <danny30lin@gmail.com>
2019-07-18 20:46:05 +0800

    callback7

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+# import module
 import json as js
+import app
+from flask import Flask, request, abort
 
-from flask import Flask, request
 app = Flask(__name__)
 
 from linebot import (
@@ -22,16 +24,27 @@ def hello():
 
 @app.route("/callback", methods = ["POST"])
 def callback():
-    
-    json_line = request.get_json()
-    print("I print something")
-    return js.dumps( json_line )
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
 
-@handler.add( MessageEvent, message = TextMessage )
+    # get request body as text
+    body = request.get_data( as_text = True )
+    app.logger.info( "Request body: " + body )
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort( 400 )
+
+    return "OK"
+
+@handler.add(MessageEvent, message = TextMessage )
 def handle_message( event ):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage( text = "Yes Yes reply" ) )
+        TextSendMessage( text = event.message.text ) )
 
 if __name__ == "__main__":
     app.run()
