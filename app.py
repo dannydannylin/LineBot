c352b181b7aee283bd5c26103787037358f033ce
unknown <danny30lin@gmail.com>
2019-07-18 15:47:38 +0800

    callback5

--- app.py
+++ app.py
@@ -13,14 +13,18 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+line_bot_api = LineBotApi("6S5y3NrPtinBzdKDiArf/AoCyJwpimZtov/uQbod/1gTtZGU+Kaapc23A5DYIpRosCBfBKk+YdqqNOXi5EmnqaqCmG4CetaCfdnkxH0QE2lKy0ulvGwF0geDSUV9qfYW1ioFU49EEbxFpzdgKGHBBQdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
 @app.route("/", methods = ["GET"])
 def hello():
     return "Hi web!!"
 
 @app.route("/callback", methods = ["POST"])
 def callback():
-    line_json = request.get_json()
-    return "line post!!"
+    print("I print something")
+    # line_json = request.get_data()
+    return "callback"
 
 if __name__ == "__main__":
     app.run()
